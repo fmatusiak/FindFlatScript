7b8b222e937b66873d6a8b1c619eeaf761d5ab85
fmatusiak <f.matusiak95@gmail.com>
2019-01-16 17:31:37 +0100

    test application in Runner

--- src/main/java/com/fmatusiak/findflat/ParseHtml.java
+++ src/main/java/com/fmatusiak/findflat/ParseHtml.java
@@ -1,25 +1,55 @@
 package com.fmatusiak.findflat;
 
-import java.io.PrintWriter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ParseHtml {
 
-    private PrintWriter printWriter;
+    public ArrayList<String> getFlatUrlForPortel(String url) {
+        ArrayList<String> flatUrlList = new ArrayList<>();
+        Document doc = null;
 
-    public void parseHtmlForPortel(){
-       /* Document doc = Jsoup.connect(site).get();
-        printWriter.println(doc.title() + "\n");
-        System.out.println(doc.title() + "\n");
+        try {
+            doc = (Document) Jsoup.connect(url).get();
+        } catch (IOException e) {
+            System.out.println("Error in read website:  " + url);
+        }
 
         Elements newsHeadlines = doc.select("div#category");
         Elements tmpHeadlines = newsHeadlines.select("div.list.plat");
         Elements Property = tmpHeadlines.select("a");
 
         for (Element headline : Property) {
+            flatUrlList.add(". " + "https://www.portel.pl" + headline.attr("href") + "\n");
+        }
 
-            printWriter.println(i + ". " + "https://www.portel.pl" + headline.attr("href") + "\n");
-            System.out.println("https://www.portel.pl" + headline.attr("href") + "\n");
-            i++;
-        }*/
+        return flatUrlList;
     }
+
+    public ArrayList<String> getFlatUrlForOlx(String url) {
+        ArrayList<String> flatUrlList = new ArrayList<>();
+        Document doc = null;
+
+        try {
+            doc = (Document) Jsoup.connect(url).get();
+        } catch (IOException e) {
+            System.out.println("Error in read website:  " + url);
+        }
+
+        Elements newsHeadlines = doc.select("tr.wrap");
+        Elements tmpProperty = newsHeadlines.select("a");
+
+        for (Element headline : tmpProperty) {
+            flatUrlList.add(headline.attr("href") + "\n");
+        }
+
+        return flatUrlList;
+    }
+
+
 }
